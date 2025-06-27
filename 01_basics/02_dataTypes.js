c5a1972928f3898341518590633254f7dc671181
PRATEEK150 <109938508+PRATEEK150@users.noreply.github.com>
2025-06-27 14:11:07 +0000

    data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,12 +1,41 @@
-"use strict" // this is used to treat all js code as newer version of js code
+// "use strict" - this is used to treat all js code as newer version of js code
+
 /* Data types-
 
-numbers
-bigInt
-string
-boolean
-null -        (type of null is object)
-undefined-    (type of undefined is undefined)
-symbol
-object
+numbers       (data type of number is number )
+bigInt        (data type of  is )
+string         (data type of string  is string )
+boolean        (data type of boolean is boolean)
+null -        (data type of null is object)
+undefined-    (data type of undefined is undefined)
+symbol         (data type of  is )
+object        (data type of  is )
 
+
+data types are of 2 types --> 
+
+1. Primitive datatypes-
+these are call-by value
+
+string, number, boolean, bigInt, null, undefined,symbol
+
+ 
+2. Reference ( Non-Primitive) Datatypes-
+  Array, objects, functions
+  # Data type of all Non-Primitive datatype is "function"
+   
+
+  Arrays-
+  const array = ["Prateek", "Swaroop", "Srivastav"]
+
+  Object-
+  let myObj = {
+      name:"Prateek";
+      age:26
+  }
+  Function- 
+  const myFunction = function(){
+      console.log("Hello World");
+  }
+
+  */
