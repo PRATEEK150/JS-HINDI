b4fd0a9b91d06d8fe74a8826b6273d99644c0517
PRATEEK150 <109938508+PRATEEK150@users.noreply.github.com>
2025-06-27 11:33:02 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict" // this is used to treat all js code as newer version of js code
+/* Data types-
+
+numbers
+bigInt
+string
+boolean
+null -        (type of null is object)
+undefined-    (type of undefined is undefined)
+symbol
+object
+
