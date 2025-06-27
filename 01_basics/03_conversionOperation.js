62e2018a24646c7ea6bfa251b470b97710e8b82f
PRATEEK150 <109938508+PRATEEK150@users.noreply.github.com>
2025-06-27 12:36:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+/*
+let score = 33
+console.log(typeof(score));
+let value = Number(score);
+console.log(value);
+let data = String(value);
+console.log(typeof(data));
+
+let marks = null;
+console.log(typeof(marks));
+console.log(marks);
+let result = Number(marks)
+console.log(typeof(result));
+console.log(result); 
+
+let outcome = true
+console.log(outcome);
+let effect = Number(outcome);
+console.log(effect);
+console.log(typeof(effect));
+console.log(); 
+*/
+
+/* "33" - if converted to number gives - 33
+    "33abc" - if converted to number gives - NaN i.e. not a number
+    true - if converted to number gives - 1
+    false - if converted to number gives - 0
+    
+
+    let isLoggedIn = 1;
+    let boolData = Boolean(isLoggedIn);
+    console.log(boolData);  
+
+    let isLoggedIn = 0;
+    let boolData = Boolean(isLoggedIn);
+    console.log(boolData); 
+
+     let isLoggedIn = "";
+    let boolData = Boolean(isLoggedIn);
+    console.log(boolData); */
