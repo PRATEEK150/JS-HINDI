376f1539a2d3b44b00acd6a4dd1c08300aa93d8b
PRATEEK150 <109938508+PRATEEK150@users.noreply.github.com>
2025-06-28 03:31:09 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,87 @@
+/*const name = "Prateek"
+const repoCount = 50
+//console.log(name + " " + repoCount);               ---- this method is outdated
+console.log(`my name is ${name} and score is ${repoCount}`); */
+
+const myName = new String('PrateekSwaroop');
+/*console.log(myName[0]);
+console.log(myName[3]);
+console.log(myName[5]);
+console.log(myName.__proto__);  --- it gives the object  
+
+console.log(myName.length);
+
+console.log(myName.toUpperCase());
+
+console.log(myName.toLowerCase());
+
+console.log(myName.charAt(5));
+
+console.log(myName.indexOf('t'));  
+
+const newName = myName.substring(0,6);
+console.log(newName);            
+
+const modName = myName.slice(-2,2)
+console.log(modName);    
+
+const extraName = "    Prateek  Swaroop   "
+console.log(extraName.trim());  */  
+
+const url = "https:// Prateek.com/Swaroopsrivastava";
+/*console.log(url.replace('p','q'));
+//console.log(url.replace('Prateek','Prakhar'));
+console.log(url.includes('Prateek'));
+console.log(url.includes('Prakhar'));   
+
+const str = "I can achieve what ever i want to achive";
+const words = str.split(' ');
+console.log(words[5]);
+
+const chars = str.split('')
+console.log(chars[4])
+console.log(chars[9]);
+console.log(chars[12]);   */
+const urltwo = "Prateek/Swaroop/Srivastav/1508"
+/*const idea = urltwo.split('/')
+console.log(idea[1]);
+console.log(idea[2]);
+console.log(idea[3]);     */
+
+const ideatwo = urltwo.split('a')
+console.log(ideatwo[0]);
+console.log(ideatwo[1]);
+console.log(ideatwo[2]);
+console.log(ideatwo[3]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
